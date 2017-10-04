9ebadfe446805c6ba6001ac1ad7ee2487c31743c
Etienne Galea <egalea.11@gmail.com>
2017-10-04 17:32:23 +0200

    migrated database to azure (modified connection string)

--- BotanikaLabs/App_Start/FilterConfig.cs
+++ BotanikaLabs/App_Start/FilterConfig.cs
@@ -7,8 +7,8 @@ namespace BotanikaLabs
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
-            filters.Add(new AuthorizeAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
+            //filters.Add(new AuthorizeAttribute());
         }
     }
 }
